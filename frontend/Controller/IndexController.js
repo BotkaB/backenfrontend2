39d2424b0acbfe798646f18147e534375d6c43db
BotkaB <botikabinaka@gmail.com>
2024-01-10 21:44:55 +0100

    szemelyek card formázva, kedvencek hozzáadása működik, kedvenceim gomb működik

--- frontend/Controller/IndexController.js
+++ frontend/Controller/IndexController.js
@@ -2,24 +2,43 @@ import DataService from "../Model/DataService.js";
 import IndexKartyaView from "../View/IndexKartyaView.js";
 
 export default class Controller {
+ 
     constructor() {
         this.dataService = new DataService()
 
+
         this.dataService.getData("szemelynyts", this.megjelenit)
-     
+        $(".kedvencek").slideToggle()
         $(window).on("kedveles", (event) => {
-           console.log(event.detail)
+            console.log(event.detail)
             this.dataService.putData("szemelynyts", event.detail)
         })
-    }
 
+
+        $(".gomb button").on("click", function () {
+            console.log("kedvencek")
+            $(".gomb").slideToggle()
+            $(".adatok").slideToggle()
+            $(".kedvencek").slideToggle()
+        })
+
+        
+        
+    }
+   
 
     megjelenit(list) {
+
+
         console.log(list)
-        for(var i=0;i<list.length; i++){
-        new IndexKartyaView(i,list[i], $(".adatok"))
-        if(list[i].kedvenc=true)
-        {new IndexKartyaView(i,list[i], $(".kedvencek"))}
+        for (var i = 0; i < list.length; i++) {
+            new IndexKartyaView(i, list[i], $(".adatok"))
+            if (list[i].kedvenc == true) {
+                new IndexKartyaView(i, list[i], $(".kedvencek"))
+
+                console.log(list[i])
+               
+            }
         }
     }
 }
--- frontend/View/IndexKartyaView.js
+++ frontend/View/IndexKartyaView.js
@@ -4,46 +4,50 @@ export default class IndexKartyaView {
     this.#obj = obj;
     this.index = index;
     this.szuloElem = szuloElem;
-    //console.log(this.#obj);
+   
+  
     this.htmlOsszerak();
 
 
   }
 
-  /* trigger(e){
-     const esemenyem=new CustomEvent(e,{detail:this.#obj})
-     window.dispatchEvent(esemenyem)
-   }*/
+
 
   htmlOsszerak() {
-    let txt = `
- 
-        <div  class="card h-100" " style="max-width: 19rem">
+   
+    let txt= `
+    
+    
+    <div class="col">
   
-        <h5 class="card-header"   >${this.#obj.nev}</h5>
+  <div  class="card h-100" " style="max-width: 19rem">
+  
+  <h5 class="card-header"   >${this.#obj.nev}</h5>
 
 <div class="card-body" >
-
 <p class="card-text">${this.#obj.szulido}</p>
 <p class="card-text">${this.#obj.szulh}</p>
 <p class="card-text">${this.#obj.anyjaneve}</p>
 </div>
 <div class="card-footer">
-
 <button class="kedvenc btn btn-outline-primary"  >Kedvencek</button>
 
 </div>
+</div>
 
-      </div>`;
+</div>`
+
+
+;
 
 
     this.szuloElem.append(txt)
 
     this.szuloElem.find(".kedvenc:last").on("click", () => {
-      this.#obj.kedvenc=true;
+      this.#obj.kedvenc = true;
       this.trigger("kedveles")
       console.log("gomb")
-      
+
     })
 
   }
